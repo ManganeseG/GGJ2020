34085f3e5895866cf2e7adb0d82399a06dcaf9c1
Quentin Decharneux <Zepalios@users.noreply.github.com>
2020-01-31 23:31:33 +0100

    Folders
    Basic Hierarchy

---
+++ Assets/Code/Script/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region public Variables
+
+    #endregion
+
+    #region _private Variables
+
+    #endregion
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
