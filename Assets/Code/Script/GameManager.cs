0443d7834f34490937cb6a3d61a35ba4dda78153
ManganeseG <36071773+ManganeseG@users.noreply.github.com>
2020-02-01 14:37:47 +0100

    timeout, gamemanager reference
    juicy animation curve and reference to the gm to stop spawning items

--- Assets/Code/Script/GameManager.cs
+++ Assets/Code/Script/GameManager.cs
@@ -4,20 +4,35 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 #region Timer
+    [Header("Timer")]
     public Text Timer;
     public float TimerValue;
+    [Header("TimeOut Text")]
+    public Text Timeout;
+    public float LerpSpeed = .5f;
+    private float lerpVal = 0f;
+    public AnimationCurve timeOutAC;
     #endregion
 
     void Start()
     {
-        
+        Timeout.enabled = false;
     }
 
     void Update()
     {
         TimerValue -= Time.deltaTime;
         Timer.text = " " + Mathf.Round(TimerValue);
+        
+        if (TimerValue <= 0)
+        {
+            TimerValue = 0f;
+            Timeout.enabled = true;
 
+            lerpVal += Time.deltaTime * LerpSpeed;
+            float timeOutEval = timeOutAC.Evaluate(lerpVal);
+            Timeout.rectTransform.localScale = new Vector3(timeOutEval, timeOutEval, timeOutEval);
+        }
 
     }
 }
