f3632146551aaed234de15181931cd1a4ea2375f
ManganeseG <36071773+ManganeseG@users.noreply.github.com>
2020-02-02 16:08:16 +0100

    statues totem

--- Assets/Code/Script/Totem.cs
+++ Assets/Code/Script/Totem.cs
@@ -10,10 +10,37 @@ public class Totem : MonoBehaviour
     public Slider constructionSlider;
     private int winner;
     public bool thereIsAWinner = false;
+    public GameObject totemOwl;
+    public GameObject totemGoat;
+    public GameObject totemAxolotl;
+    public GameObject totemCat;
+    private GameObject totemselected;
+    public DummyMove playerDummy;
 
     void Start()
     {
         constructionSlider.value = 0;
+        switch (playerDummy.CharacterName)
+        {
+            case DummyMove.CharaName.Cat:
+                totemselected = Instantiate(totemCat, gameObject.transform.position, gameObject.transform.rotation);
+                totemselected.transform.parent = gameObject.transform;
+                break;
+            case DummyMove.CharaName.Goat:
+                totemselected = Instantiate(totemGoat, gameObject.transform.position, gameObject.transform.rotation);
+                totemselected.transform.parent = gameObject.transform;
+                break;
+            case DummyMove.CharaName.Axolotl:
+                totemselected = Instantiate(totemAxolotl, gameObject.transform.position, gameObject.transform.rotation);
+                totemselected.transform.parent = gameObject.transform;
+                break;
+            case DummyMove.CharaName.Owl:
+                totemselected = Instantiate(totemOwl, gameObject.transform.position, gameObject.transform.rotation);
+                totemselected.transform.parent = gameObject.transform;
+                break;
+            default:
+                break;
+        }
     }
     
 
